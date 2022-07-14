e908f22cbb8e294c81ce696b1e955125c1ce3ebb
Olatunde Abodunrin <abo2gomal92@gmail.com>
2022-07-14 05:41:26 -0700

    pointers, arrays and strings tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,8 +12,8 @@ int divisor = 1, i, resp;
 
 if (n < 0)
 {
-	_putchar('-');
-	n *= -1;
+        _putchar('-');
+        n *= -1;
 }
 
 for (i = 0; n / divisor > 9; i++, divisor *= 10)
@@ -21,8 +21,8 @@ for (i = 0; n / divisor > 9; i++, divisor *= 10)
 
 for (; divisor >= 1; n %= divisor, divisor /= 10)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+        resp = n / divisor;
+        _putchar('0' + resp);
 
 }
 
